ac6a987b7e9d776336a9e01b1e6cb2cea88bd9c4
RebecaApolinario <rebecappolinario@gmail.com>
2024-09-03 19:28:02 -0300

    Create questao4.py

---
+++ questao4.py
@@ -0,0 +1,19 @@
+sp = 67836.43
+rj = 36678.66
+mg = 29229.88
+es = 27165.48
+outros = 19849.53 
+
+total = sp + rj + mg + es + outros
+
+sp = sp * 100 / total
+rj = rj * 100 / total
+mg = mg * 100 / total
+es = es * 100 / total
+outros = outros * 100 / total 
+
+print(f"sp: {sp:.2f}%")
+print(f"rj: {rj:.2f}%")
+print(f"mg: {mg:.2f}%")
+print(f"es: {es:.2f}%")
+print(f"outros: {outros:.2f}%")
